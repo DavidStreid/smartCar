010671e3ce783ae78a586986c82c0139337d8809
David Streid <dcs2153@columbia.edu>
2017-08-29 19:59:45 -0700

    fixed callback of invalidId tests interfering with valid id tests

--- test/smartCartTest.js
+++ test/smartCartTest.js
@@ -5,6 +5,39 @@ var request   = require('request');
 var base = "http://localhost:3000/vehicles/"
 var valid_id = "1234/"
 describe("SmartCar API", function() {
+
+  // NOTE - Test invalid ids first as their callback (with quicker latency) can interfere w/ other tests
+  describe("SmartCar API with invalid id", function() {
+    // Check Invalid id
+    let invalid_id = "4321/";
+    let host = base + invalid_id;
+    let endpoints = ['','doors','fuel','battery']
+    for(let i = 0; i<endpoints.length; i++){
+      let e = endpoints[i];
+      let url = host + e;
+      describe("Endpoint: " + url + " with invalidID", function(){
+        it("returns 404 & Vehicle Not Found", function(done){
+          request(url, function(error, response, body) {
+            expect(JSON.parse(body).status).to.equal(404);
+            expect(JSON.parse(body).message).to.equal("Vehicle Not Found");
+            done();
+          });
+        })
+      });
+    }
+    let url = host + "engine"
+    describe("Endpoint: " + url + " with invalidID", function(){
+      it("returns 404 & Vehicle Not Found", function(done){
+        let obj = { "action": "start" }
+        request.post(url, obj, function (error, response, body) {
+          expect(JSON.parse(body).status).to.equal(404);
+          expect(JSON.parse(body).message).to.equal("Vehicle Not Found");
+          done();
+        });
+      })
+    })
+  })
+
   describe("GM: getVehicleInfoService, SmartCar: /", function() {
     it("gets vehicle info if valid ID", function(done) {
 	  let url = base + valid_id
@@ -34,58 +67,26 @@ describe("SmartCar API", function() {
       });
     });
   });
-
-
-
   describe("GM:getEnergyService, SmartCar API: /fuel & /battery", function() {
-  	let energyType = [["fuel", "tankLevel","1234"], ["battery", "batteryLevel","1235"]];
-  	for (let i = 0; i<energyType.length; i++){
-  		let type = energyType[i][0];
-  		let field = energyType[i][1];
-  		let id = energyType[i][2];
+  	let energyType = [["fuel", "tankLevel","1234/"], ["battery", "batteryLevel","1235/"]];
+    it("gets energy levels as float", function(done) {
+    	for (let i = 0; i<energyType.length; i++){
+    		let type = energyType[i][0];
+    		let field = energyType[i][1];
+    		let id = energyType[i][2];
 
-	    it("gets " + type + " levels as float", function(done) {
-		  let url = base + id + type;
+  	    
+  		  let energy_url = base + id + type;
 
-
-		  // Populate initial door locked status
-	      request(url, function(error, response, body) {
-	        expect(response.statusCode).to.equal(200);
-			let pct = JSON.parse(body)[field].percent;
-			expect(0<=pct && pct <= 100).to.equal(true);         
-	      });
-	      done();
-	    });
-  	}
+  		  // Populate initial door locked status
+  	      request(energy_url, function(error, response, body) {
+  	        expect(response.statusCode).to.equal(200);
+            let pct = JSON.parse(body)[field].percent;
+            expect(0<=pct && pct <= 100).to.equal(true); 
+  	      });
+    	  }
+        done();    
+    });
   });
 
-
-  // Check Invalid id
-  let invalid_id = "4321/";
-  let host = base + invalid_id;
-  let endpoints = ['','doors','fuel','battery']
-  for(let i = 0; i<endpoints.length; i++){
-    let e = endpoints[i];
-    let url = host + e;
-  	describe("Endpoint: " + url + " with invalidID", function(){
-  	  it("returns 404 & Vehicle Not Found", function(done){
-        request(url, function(error, response, body) {
-          expect(JSON.parse(body).status).to.equal(404);
-          expect(JSON.parse(body).message).to.equal("Vehicle Not Found");
-          done();
-        });
-  	  })
-    });
-  }
-  let url = host + "engine"
-  describe("Endpoint: " + url + " with invalidID", function(){
-    it("returns 404 & Vehicle Not Found", function(done){
-      let obj = { "action": "start" }
-      request.post(url, obj, function (error, response, body) {
-        expect(JSON.parse(body).status).to.equal(404);
-        expect(JSON.parse(body).message).to.equal("Vehicle Not Found");
-        done();
-      });
-    })
-  })
 });
